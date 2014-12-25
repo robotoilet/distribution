2929414cba6b4f188a88950fa42c20b0470760cc
Alecs Geuder <al.geuder@gmail.com>
2014-12-25 15:40:55 +0100

    bugfix: one post per series

--- test/test_distribute.js
+++ test/test_distribute.js
@@ -9,17 +9,24 @@ var should = require('should')
 describe('distribute_mainfunction', function() {
   var http_api;
   var dbUrl = "/db/" + config.dbName + "/series";
-  var dataString = "(d 123 456)(d 789 10)";
+  var dataString = "(d 123 456)(a 12 34)(d 789 10)";
   var dataObj = [
     {
       name: 'punterX_siteX_sensorschmensor',
       columns: ['time', 'line'],
       points: [[123000, 456], [789000, 10]]
+    },
+    {
+      name: 'punterX_siteX_SensorX',
+      columns: ['time', 'line'],
+      points: [[12000, 34]]
     }
   ];
   beforeEach(function(){
     http_api = nock(config.httpApi)
-      .post(dbUrl, dataObj)
+      .post(dbUrl, dataObj[0])
+      .reply(200)
+      .post(dbUrl, dataObj[1])
       .reply(200);
   });
   it('should convert a data string and post as json to the http api', function(done) {
@@ -28,10 +35,14 @@ describe('distribute_mainfunction', function() {
       username: 'punterX',
       password:'punterX'
     };
+    var counter = 0;
     function nockHappy(msg) {
       logger.debug(msg);
-      http_api.done();
-      done()
+      counter ++;
+      if (counter === 2) {
+        http_api.done();
+        done();
+      }
     }
     distribute(auth, dataString, logger.error, nockHappy);
   });
